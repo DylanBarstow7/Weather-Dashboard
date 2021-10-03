c7ca79d4ed674cbcbbbfc2464dfd6529f2bb4c99
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-02 23:29:42 -0500

    creating script variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,16 @@
 function init(){
+	const inputEl = document.getElementById("city-input");
+	const searchEl = document.getElementById("search-button");
+	const clearEl = document.getElementById("clear-history");
+	const nameEl = document.getElementById("city-name");
+	const currentPicEl = document.getElementById("current-pic");
+	const currentTempEl = document.getElementById("temperature");
+	const currentHumidityEl = document.getElementById("humidity");4
+	const currentWindEl = document.getElementById("wind-speed");
+	const currentUVEl = document.getElementById("UV-index");
+	const historyEl = document.getElementById("history");
+	let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+	console.log(searchHistory);
 
 	const APIKey = "c2b57ce6c568bc5d04209e2700a7de85";
 
@@ -61,6 +73,35 @@ function init(){
 						})
 				});
 		}
-
+		searchEl.addEventListener("click",function() {
+			const searchTerm = inputEl.value;
+			getWeather(searchTerm);
+				searchHistory.push(searchTerm);
+				localStorage.setItem("search",JSON.stringify(searchHistory));
+				renderSearchHistory();
+		})
+		clearEl.addEventListener("click",function() {
+			searchHistory = []; 
+			renderSearchHistory();})
+			function k2f(K) {
+				return Math.floor((K - 273.15) *1.8 +32);
+		}
+		function renderSearchHistory() {
+				historyEl.innerHTML = "";
+				for (let i=0; i<searchHistory.length; i++) {
+						const historyItem = document.createElement("input");
+						historyItem.setAttribute("type","text");						historyItem.setAttribute("readonly",true);
+						historyItem.setAttribute("class", "form-control d-block bg-white");
+						historyItem.setAttribute("value", searchHistory[i]);
+						historyItem.addEventListener("click",function() {
+							getWeather(historyItem.value);
+					})
+				historyEl.append(historyItem);				
+			}
+			renderSearchHistory();
+			if (searchHistory.length > 0) {
+				getWeather(searchHistory[searchHistory.length - 1]);
+		}
+		}
 }			
 init();
